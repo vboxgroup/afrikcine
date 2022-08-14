b8b4aa34c5968c9cdc3b062750d1eb96014d63b5
vboxgroup <dev.vboxgroup@gmail.com>
2022-08-14 16:52:42 +0000

    config folder commit

---
+++ config/bundles.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    DovStone\Bundle\SymfonyBlogAdminBundleMyNoSQLBased\DovStoneSymfonyBlogAdminBundleMyNoSQLBased::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+];
